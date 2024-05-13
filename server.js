6a6664ba31884169dfbe07302ef2a09f24e4f21d
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-05-13 15:16:33 -0400

    setup

---
+++ server.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const { json } = require('body-parser')
+const PORT = process.env.PORT || 3000;
+
+
+const notesData = require('./db/db.json')
+
+
+// terminal colors for messages
+const green = '\x1b[32m%s\x1b[0m';
+const red = '\x1b[31m%s\x1b[0m';
+yellow = '\x1b[33m%s\x1b[0m';
+
+// CRUD Ports
+// Create note
+
+// Read note
+// retrieve main page
+app.get("/", (req, res) => {
+    console.log( green, `😀 default routs reached!`)
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+// Get notes
+app.get("/notes", (req, res) => {
+    console.log(green, `😀 notes accessed`)
+    // res.json(notesData);
+})
+// Update note
+
+// Delete note
+
+// express functional code
+app.use(express.static("./public"));
+app.use(express.json())
+
+app.listen(PORT, ()=> {
+    console.log(green, `😎 Server is running on port ${PORT}`)
+})
